a70c2c0beec0087f66f8097d2f46556fc1323ee2
renivermartins <r.kennyster@icloud.com>
2023-08-06 21:01:52 -0300

    inicio do projeto.

---
+++ CRUD/src/App.java
@@ -0,0 +1,20 @@
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        /**
+         * 
+         * Incluir o fluxo:
+         * importar via arquivo .csv
+         * Cadastrar usuario na lista (nome, idade, telefone, email)
+         * Consultar usuarios cadastrados
+         * Consultar nome ou telefone ou e-mail
+         * alterar usuario cadastrado
+         * deletar usuario cadastrado
+         *
+         **/
+
+
+    }
+}
